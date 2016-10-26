0ca799e8d547db484fa03fa0ad2a36fa043b60a1
Syed Zaman <zamandiue44@gmail.com>
2016-10-26 19:50:09 +0600

    Update

--- T-29.01.php
+++ T-29.01.php
@@ -42,28 +42,30 @@ $fontcolor = "#343F94";
 		<hr />
 		PHP From Validation
 		<hr />
+		
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
 		<table>
 			<tr>
 				<td>Name: </td>
-				<td><input type="text" name="name" /></td>
+				<td><input type="text" name="name" required /></td>
 			</tr>
 			<tr>
 				<td>E-mail: </td>
-				<td><input type="email" name="email" /></td>
+				<td><input type="email" name="email" required /></td>
 			</tr>
 			<tr>
 				<td>Website: </td>
-				<td><input type="text" name="website" /></td>
+				<td><input type="text" name="website" required /></td>
 			</tr>
 			<tr>
 				<td>Comment: </td>
-				<td><textarea name="comment" id="" cols="30" rows="10"></textarea></td>
+				<td><textarea name="comment" id="" cols="30" rows="10" required></textarea></td>
 			</tr>
 			<tr>
 				<td>Gender: </td>
 				<td>
-					<input type="radio" name="gender" value="Female" />Female
-					<input type="radio" name="gender" value="Male" />Male
+					<input type="radio" name="gender" value="Female"/>Female
+					<input type="radio" name="gender" value="Male"  />Male
 				</td>
 			</tr>
 			<tr>
@@ -71,14 +73,37 @@ $fontcolor = "#343F94";
 				<td><input type="submit" name="submit" value="Submit" /></td>
 			</tr>
 		</table>
+		</form>
 			<?php 
+			$name = $email = $website = $comment = $gender = "";
+				if($_SERVER['REQUEST_METHOD']=="POST"){
+					$name= Validate($_POST["name"]);
+					$email= Validate($_POST["email"]);
+					$website= Validate($_POST["website"]);
+					$comment= Validate($_POST["comment"]);
+					$gender= Validate($_POST["gender"]);					
+						echo "Name: ".$name."<br />";
+						echo "E-mail: ".$email."<br />";
+						echo "Website: ".$website."<br />";
+						echo "Comment: ".$comment."<br />";
+						echo "Gender: ".$gender."<br />";
+						}
+				function Validate($data){
+					$data= trim($data);
+					$data= stripcslashes($data);
+					$data= htmlspecialchars($data);
+					return $data;				
+				}
+				
+				
 			
 			?>
 			
 		</section>
 		<section class="footeroption">
-			<a href="T-23.01.php"><h2><?php echo "Zaman Web Education";?></h2></a>
+			<h2><?php echo "Zaman Web Education";?></h2>
 		</section>
+		<a href="T-30.01.php">T-30.01.php</a>
 	</div>
 
 	
